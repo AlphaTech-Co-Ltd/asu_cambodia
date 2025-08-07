fd4dc6c141835acdb45639709b178d60339bc991
SkyHeng168 <sreangtongheng009@gmail.com>
2025-08-07 17:03:40 +0700

    Our Complete Mession

---
+++ src/compenents/Home/OurStudent_And_Tour/OurStudent_And_Tour.tsx
@@ -0,0 +1,15 @@
+
+export default function OurStudent_And_Tour(){
+    return (
+        <div className={"pt-16 pb-16"}>
+            <div className={"text-center"}>
+                <h1 className={"uppercase text-lg font-bold sm:text-xl md:text-2xl text-blue-950 dark:text-white"}>
+                    Our Student And Tour
+                </h1>
+                <h1 className={"text-xl sm:text-3xl md:text-4xl mt-1 font-bold"}>
+                    We helped many get U.S<br/> visas for study and travel
+                </h1>
+            </div>
+        </div>
+    );
+}
