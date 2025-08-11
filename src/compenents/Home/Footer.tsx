dbc1e99ea01ece5dd67a63ec83c6f366246bc6b9
SkyHeng168 <sreangtongheng009@gmail.com>
2025-08-11 08:29:12 +0700

    Prisma

--- src/compenents/Home/Footer.tsx
+++ src/compenents/Home/Footer.tsx
@@ -1,79 +1,114 @@
 import Link from "next/link";
 import Image from "next/image";
-import {FaFacebook, FaInstagram, FaTelegram, FaYoutube} from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTelegram, FaYoutube } from "react-icons/fa";
 
 export default function FooterPage() {
-    return (
-        <div className="pt-16 pb-16 bg-blue-950">
-            <div className="w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 items-start">
-                {/* Logo & Description */}
-                <div>
-                    <div className="flex items-center space-x-2">
-                        <Link href="/" className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
-                            <Image
-                                src="/Logo/Logo.png"
-                                alt="Logo"
-                                priority
-                                width={40}
-                                height={40}
-                                className="rounded-full object-cover"
-                            />
-                        </Link>
-                        <h1 className="text-xl font-bold text-white md:text-2xl">
-                            ASU Cambodia
-                        </h1>
-                    </div>
+  return (
+    <div className="pt-16 pb-16 bg-blue-950">
+      <div className="w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 items-start">
+        {/* Logo & Description */}
+        <div>
+          <div className="flex items-center space-x-2">
+            <Link
+              href="/"
+              className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
+              <Image
+                src="/Logo/Logo_New.png"
+                alt="Logo"
+                priority
+                width={40}
+                height={40}
+                className="rounded-full object-cover"
+              />
+            </Link>
+            <h1 className="text-xl font-bold text-white md:text-2xl">
+              ASU Cambodia
+            </h1>
+          </div>
 
-                    <p className="mt-4 text-gray-200 font-medium">
-                        Ambitious Students Ubiquitous is the exclusive representative of Angelo State University in Cambodia.
-                    </p>
+          <p className="mt-4 text-gray-200 font-medium">
+            Ambitious Students Ubiquitous is the exclusive representative of
+            Angelo State University in Cambodia.
+          </p>
 
-                    {/* Social Icons */}
-                    <div className="mt-6 flex items-center space-x-3">
-                        <a href="https://www.facebook.com/ambitiousstudentsubiquitous" rel="noopener noreferrer" target="_blank" className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
-                            <FaFacebook className="text-[#1877F2]" /> {/* Facebook Blue */}
-                        </a>
+          {/* Social Icons */}
+          <div className="mt-6 flex items-center space-x-3">
+            <a
+              href="https://www.facebook.com/ambitiousstudentsubiquitous"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
+              <FaFacebook className="text-[#1877F2]" /> {/* Facebook Blue */}
+            </a>
 
-                        <a href="https://t.me/globalpromgram_asu" target="_blank" rel="noopener noreferrer" className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
-                            <FaTelegram className="text-[#0088cc]" /> {/* Telegram Blue */}
-                        </a>
+            <a
+              href="https://t.me/globalpromgram_asu"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
+              <FaTelegram className="text-[#0088cc]" /> {/* Telegram Blue */}
+            </a>
 
-                        <a href="https://www.instagram.com/ambitiousstudentsubiquitous" target="_blank" rel="noopener noreferrer" className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
-                            <FaInstagram className="text-[#E4405F]" /> {/* Instagram Pink-Red */}
-                        </a>
+            <a
+              href="https://www.instagram.com/ambitiousstudentsubiquitous"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
+              <FaInstagram className="text-[#E4405F]" />{" "}
+              {/* Instagram Pink-Red */}
+            </a>
 
-                        <a href="https://www.youtube.com/@angelostate" target="_blank" rel="noopener noreferrer" className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
-                            <FaYoutube className="text-red-700" /> {/* TikTok default black, or use #010101 */}
-                        </a>
-                    </div>
-                </div>
-                <div className={"space-y-5"}>
-                    <h1 className={"text-xl font-bold text-white"}>
-                        Company
-                    </h1>
-                    <p className={"footer__link"}>About US</p>
-                    <p className={"footer__link"}>News & Press</p>
-                    <p className={"footer__link"}>Our Student</p>
-                    <p className={"footer__link"}>Careers</p>
-                </div>
-            </div>
-            <div className={"mt-8 w-[80%] mx-auto border-t pt-8 flex flex-col md:flex-row justify-between items-center text-gray-500 text-sm"}>
-                <p className={"text-center text-white md:text-left"}>
-                    Copyright © 2025 AUS Cambodia. All rights reserved
-                </p>
-                <div className={"flex items-center text-white space-x-4 mt-4 md:mt-0"}>
-                    <span>Social Media:</span>
-                    <a href="https://www.facebook.com/ambitiousstudentsubiquitous" rel="noopener noreferrer" target="_blank" className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
-                        <FaFacebook className="text-[#1877F2]" /> {/* Facebook Blue */}
-                    </a>
-                    <a href="https://t.me/globalpromgram_asu" target="_blank" rel="noopener noreferrer" className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
-                        <FaTelegram className="text-[#0088cc]" /> {/* Telegram Blue */}
-                    </a>
-                    <a href="https://www.instagram.com/ambitiousstudentsubiquitous" target="_blank" rel="noopener noreferrer" className="w-8 h-8 flex items-center bg-white justify-center rounded hover:bg-gray-200 transition">
-                        <FaInstagram className="text-[#E4405F]" /> {/* Instagram Pink-Red */}
-                    </a>
-                </div>
-            </div>
+            <a
+              href="https://www.youtube.com/@angelostate"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
+              <FaYoutube className="text-red-700" />{" "}
+              {/* TikTok default black, or use #010101 */}
+            </a>
+          </div>
         </div>
-    );
+        <div className={"space-y-5"}>
+          <h1 className={"text-xl font-bold text-white"}>Company</h1>
+          <p className={"footer__link"}>About US</p>
+          <p className={"footer__link"}>News & Press</p>
+          <p className={"footer__link"}>Our Student</p>
+          <p className={"footer__link"}>Careers</p>
+        </div>
+      </div>
+      <div
+        className={
+          "mt-8 w-[80%] mx-auto border-t pt-8 flex flex-col md:flex-row justify-between items-center text-gray-500 text-sm"
+        }>
+        <p className={"text-center text-white md:text-left"}>
+          Copyright © 2025 AUS Cambodia. All rights reserved
+        </p>
+        <div className={"flex items-center text-white space-x-4 mt-4 md:mt-0"}>
+          <span>Social Media:</span>
+          <a
+            href="https://www.facebook.com/ambitiousstudentsubiquitous"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
+            <FaFacebook className="text-[#1877F2]" /> {/* Facebook Blue */}
+          </a>
+          <a
+            href="https://t.me/globalpromgram_asu"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-8 h-8 flex items-center justify-center bg-white rounded hover:bg-gray-200 transition">
+            <FaTelegram className="text-[#0088cc]" /> {/* Telegram Blue */}
+          </a>
+          <a
+            href="https://www.instagram.com/ambitiousstudentsubiquitous"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-8 h-8 flex items-center bg-white justify-center rounded hover:bg-gray-200 transition">
+            <FaInstagram className="text-[#E4405F]" />{" "}
+            {/* Instagram Pink-Red */}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/compenents/Home/nav/Navbar.tsx
+++ src/compenents/Home/nav/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { NavLinks } from "@/constant_components/constants"; // adjust path
+import { NavLinks } from "@/constant_components/constants";
 import Link from "next/link";
 import { HiBars3 } from "react-icons/hi2";
 import { useEffect, useState } from "react";
@@ -9,89 +9,99 @@ import LanguageChange from "@/compenents/Home/Language_Change";
 import ThemeToggle from "@/constant_components/Helper/ThemeToggle";
 
 type Props = {
-    openNav: () => void;
+  openNav: () => void;
 };
 
 export default function NavBar({ openNav }: Props) {
-    const [navBg, setNavBg] = useState(false);
+  const [navBg, setNavBg] = useState(false);
 
-    useEffect(() => {
-        const handler = () => {
-            if (window.scrollY >= 90) setNavBg(true);
-            else setNavBg(false);
-        };
-        window.addEventListener("scroll", handler);
-        return () => window.removeEventListener("scroll", handler);
-    }, []);
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      else setNavBg(false);
+    };
+    window.addEventListener("scroll", handler);
+    return () => window.removeEventListener("scroll", handler);
+  }, []);
 
-    return (
-        <div
-            className={`transition-all ${
-                navBg ? "bg-blue-950 shadow-md" : "fixed"
-            } duration-200 h-[12vh] z-[100] fixed w-full bg-blue-800`}
-        >
-            <div className={"flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto"}>
-                {/* Logo */}
-                <div className={"flex items-center space-x-2"}>
-                    <div className={"w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col"}>
-                        <Link href="/">
-                            <Image
-                                src="/Logo/Logo.png"
-                                alt="Logo"
-                                priority
-                                width={40}
-                                height={40}
-                                className="rounded-full object-cover"
-                            />
-                        </Link>
-                    </div>
-                    <h1 className={"text-xl font-bold text-white hidden sm:block md:text-2xl"}>ASU Cambodia</h1>
-                </div>
+  return (
+    <div
+      className={`transition-all ${
+        navBg ? "bg-blue-950 shadow-md" : "fixed"
+      } duration-200 h-[12vh] z-[100] fixed w-full bg-blue-800`}>
+      <div
+        className={
+          "flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto"
+        }>
+        {/* Logo */}
+        <div className={"flex items-center space-x-2"}>
+          <div
+            className={
+              "w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col"
+            }>
+            <Link href="/">
+              <Image
+                src="/Logo/Logo_New.png"
+                alt="Logo"
+                priority
+                width={40}
+                height={40}
+                className="rounded-full object-cover"
+              />
+            </Link>
+          </div>
+          <h1
+            className={
+              "text-xl font-bold text-white hidden sm:block md:text-2xl"
+            }>
+            ASU Cambodia
+          </h1>
+        </div>
 
-                {/* Desktop Nav Links */}
-                <div className="hidden lg:flex items-center space-x-6">
-                    {NavLinks.map((link) => (
-                        <Link
-                            key={link.id}
-                            href={link.url}
-                            className="group relative text-white text-base font-medium transition-colors duration-300"
-                        >
+        {/* Desktop Nav Links */}
+        <div className="hidden lg:flex items-center space-x-6">
+          {NavLinks.map((link) => (
+            <Link
+              key={link.id}
+              href={link.url}
+              className="group relative text-white text-base font-medium transition-colors duration-300">
               <span className="flex items-center space-x-1 hover:text-yellow-500 duration-300">
                 {link.icon}
-                  <span className={"hover:text-yellow-500 duration-300"}>{link.label}</span>
+                <span className={"hover:text-yellow-500 duration-300"}>
+                  {link.label}
+                </span>
               </span>
-                            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-yellow-700 transition-all duration-300 group-hover:w-full"></span>
-                        </Link>
-                    ))}
-                </div>
+              <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-yellow-700 transition-all duration-300 group-hover:w-full"></span>
+            </Link>
+          ))}
+        </div>
 
-                {/* Login Button */}
-                <div className="hidden sm:flex items-center space-x-4">
-                    <Link
-                        href="/LoginAndRegister"
-                        className="relative inline-flex w-30 h-9 items-center justify-center px-4 py-2 sm:px-6 sm:py-2.5 md:px-8 md:py-3
+        {/* Login Button */}
+        <div className="hidden sm:flex items-center space-x-4">
+          <Link
+            href="/LoginAndRegister"
+            className="relative inline-flex w-30 h-9 items-center justify-center px-4 py-2 sm:px-6 sm:py-2.5 md:px-8 md:py-3
                overflow-hidden font-semibold text-white
                bg-yellow-500 rounded-lg shadow-md group
                transition-all duration-300 ease-in-out
-               text-sm sm:text-base md:text-lg hover:bg-yellow-700"
-                    >
-                        <span className="absolute inset-0 bg-white rounded-lg transform scale-0 group-hover:scale-150 transition-transform duration-500 ease-in-out"></span>
-                        <span className="relative z-10 group-hover:text-yellow-500 transition-colors duration-300 text-sm ease-in-out">
+               text-sm sm:text-base md:text-lg hover:bg-yellow-700">
+            <span className="absolute inset-0 bg-white rounded-lg transform scale-0 group-hover:scale-150 transition-transform duration-500 ease-in-out"></span>
+            <span className="relative z-10 group-hover:text-yellow-500 transition-colors duration-300 text-sm ease-in-out">
               Login
             </span>
-                    </Link>
-                </div>
-
-                {/* Theme toggle, language, and hamburger */}
-                <div className="flex items-center space-x-4">
-                    <ThemeToggle />
-                    <LanguageChange />
-                    <HiBars3
-                        onClick={openNav}
-                        className="w-8 h-8 text-white cursor-pointer lg:hidden transition-transform duration-200 hover:scale-110"
-                    />
-                </div>
-            </div>
+          </Link>
         </div>
-    );
+
+        {/* Theme toggle, language, and hamburger */}
+        <div className="flex items-center space-x-4">
+          <ThemeToggle />
+          <LanguageChange />
+          <HiBars3
+            onClick={openNav}
+            className="w-8 h-8 text-white cursor-pointer lg:hidden transition-transform duration-200 hover:scale-110"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
